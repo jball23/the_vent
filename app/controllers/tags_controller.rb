5534f819d27d47f44673fc19a676a38767dd7554
Jordan Ballard <ballard.jordan@gmail.com>
2016-04-28 00:06:32 -0400

    added a tag show page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,10 @@
 class TagsController < ApplicationController
 
+  def index
+    @tags = Tag.all
+    @vents = Vent.all
+  end
+
   def show
     @vent = Vent.find(params[:id])
     @tags = Tag.all
--- app/models/vent.rb
+++ app/models/vent.rb
@@ -4,4 +4,5 @@ class Vent < ActiveRecord::Base
   has_many :taggings
   has_many :tags, :through => :taggings
 
+  validates :name, :body, presence: true
 end
